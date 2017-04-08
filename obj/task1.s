eb2c6b8bac5f177b72756fe579c06664e1a2a9a2
Alon Blumenreich <alon@mail.com>
2017-04-08 19:40:04 +0300

    still working, the 'or' operator is not working well

--- obj/task1.s
+++ obj/task1.s
@@ -19,7 +19,28 @@ my_func:
 	mov ecx, dword [ebp+8]	; Get argument (pointer to string)
 
 ;       Your code should be here...
+	mov edx, LC1
 
+my_loop:
+	cmp byte [ecx], 0
+	je continue
+
+	mov al, byte [ecx]
+	inc ecx
+	mov bl, byte [ecx]
+	inc ecx
+	shl al, 4
+	or al, bl
+	mov byte [edx], al
+	inc edx
+	mov byte [edx], bl
+	inc edx
+	;add ecx, 2
+	jmp my_loop
+
+continue:
+	mov byte [edx], 0
+;       Your code should be here...
 	push	LC1		; Call printf with 2 arguments: pointer to str
 	push	LC0		; and pointer to format string.
 	call	printf
