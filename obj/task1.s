55144fe31a3dccdac7bcf181bbe542f71e50f585
Alon Blumenreich <alon@mail.com>
2017-04-09 19:18:30 +0300

    Finished task1 hopefully

--- obj/task1.s
+++ obj/task1.s
@@ -19,28 +19,62 @@ my_func:
 	mov ecx, dword [ebp+8]	; Get argument (pointer to string)
 
 ;       Your code should be here...
+
 	mov edx, LC1
 
 my_loop:
-	cmp byte [ecx], 0
+	cmp byte [ecx], 10
 	je continue
 
-	mov al, byte [ecx]
-	inc ecx
-	mov bl, byte [ecx]
-	inc ecx
+	mov ax, word [ecx]
+	cmp al, '9'
+	jbe conv_num_al
+	cmp al, 'Z'
+	jbe conv_high_al
+
+	sub al, 87 ;it is 'a-z'
+
+cont_loop1:
+
+	cmp ah, '9'
+	jbe conv_num_ah
+	cmp ah, 'Z'
+	jbe conv_high_ah
+
+	sub ah, 87 ;it is 'a-z'
+
+cont_loop2:
+
 	shl al, 4
-	or al, bl
+	or al, ah
 	mov byte [edx], al
 	inc edx
-	mov byte [edx], bl
-	inc edx
-	;add ecx, 2
+	add ecx, 2
 	jmp my_loop
+
+
+conv_num_al:
+	sub al, 48
+	jmp cont_loop1
+
+conv_high_al:
+	sub al, 55
+	jmp cont_loop1
+
+conv_num_ah:
+	sub ah, 48
+	jmp cont_loop2
+
+conv_high_ah:
+	sub ah, 55
+	jmp cont_loop2
+
 
 continue:
 	mov byte [edx], 0
+
 ;       Your code should be here...
+
 	push	LC1		; Call printf with 2 arguments: pointer to str
 	push	LC0		; and pointer to format string.
 	call	printf
