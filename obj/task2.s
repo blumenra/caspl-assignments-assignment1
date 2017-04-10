861385d1aa87f7b348caec48ec772dba0d9827ac
Alon Blumenreich <alon@mail.com>
2017-04-10 17:52:17 +0300

    Still working

--- obj/task2.s
+++ obj/task2.s
@@ -1,18 +1,51 @@
-section .rodata:
-	format: DD "%d", 10, 0
+section .rodata
+format:
+	DD "%d", 10, 0 ;format integer
 
-section .bss:
-	ans: RESB 4 ; hold place for integer answer
+error:
+	DB "x or k, or both are off range\n", 30, 0 ;format integer
 
-section .text:
-	;align 16
+section .bss
+ans: 
+	RESB 4 ; hold place for integer answer (4 bytes)
+
+section .text
+	align 16
 	global calc_div
 	extern printf
 
 calc_div:
+	push ebp
+	mov ebp, esp
+	pushad		; backup registers
+	pushfd		; backup flags
 
+	mov ecx, dword [ebp+8]
+	push ecx
+	mov ecx, dword [ebp+4]
+	push ecx
+
+	call check	; return value on dl
+	cmp dl, 1
+	jnz invalid
+
+	mov dword [ans], ecx
+	push dword [ans]
+	push format
+	call printf
+	
+	add esp, 8
+	popfd		; restore flags register
+	popad		; restore registers
+	pop ebp
+	
 	ret
-
 
 check:
+	mov dl, 0
 	ret
+
+
+invalid:
+	push error
+	printf
