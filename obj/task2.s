8847eaed415335b5b79d27f7814f66f3b348f31e
Alon Blumenreich <alon@mail.com>
2017-04-11 15:15:31 +0300

    Implemented the 'invalid' label and the stack flow is working fine

--- obj/task2.s
+++ obj/task2.s
@@ -2,8 +2,9 @@ section .rodata
 format:
 	DD "%d", 10, 0 ;format integer
 
+section .data
 error:
-	DB "x or k, or both are off range\n", 30, 0 ;format integer
+	DB "x or k, or both are off range", 30, 0 ;format integer
 
 section .bss
 ans: 
@@ -21,19 +22,22 @@ calc_div:
 	pushfd		; backup flags
 
 	mov ecx, dword [ebp+8]
-	push ecx
-	mov ecx, dword [ebp+4]
-	push ecx
+	;push ecx
+	;mov ecx, dword [ebp+4]
+	;push ecx
 
 	call check	; return value on dl
-	cmp dl, 1
+	cmp dl, 0
 	jnz invalid
 
 	mov dword [ans], ecx
 	push dword [ans]
 	push format
 	call printf
+	;add esp, 8
 	
+
+clear_stack:
 	add esp, 8
 	popfd		; restore flags register
 	popad		; restore registers
@@ -47,5 +51,8 @@ check:
 
 
 invalid:
+	mov byte [error+29], 10
 	push error
-	printf
+	call printf
+	add esp, 4
+	jmp clear_stack
