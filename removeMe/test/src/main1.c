3d1732b27a0ee79fc2556039d595c06a169c796f
Alon Blumenreich <alon@mail.com>
2017-04-17 10:16:21 +0300

    added tests to cover all valid ascii letters for task1

--- removeMe/test/src/main1.c
+++ removeMe/test/src/main1.c
@@ -15,6 +15,19 @@ int main(int argc, char** argv)
   test_it("33343536\n", 9, "3456");
   test_it("4c656d6f6e\n", 11, "Lemon");
   test_it("373E393D46616C7365\n", 19, "7>9=False");
+  
+  test_it("21202325272829\n", 15, "! #%'()");
+  test_it("2a2A2b2B2c2C2d2D2e2E2f2F\n", 25, "**++,,--..//");
+  test_it("30313233343536373839\n", 21, "0123456789");
+  test_it("3a3A3b3B3c3C3d3D3e3E3f3F\n", 25, "::;;<<==>>??");
+  test_it("40414243444546474849\n", 21, "@ABCDEFGHI");
+  test_it("4a4A4b4B4c4C4d4D4e4E4f4F\n", 25, "JJKKLLMMNNOO");
+  test_it("50515253545556575859\n", 21, "PQRSTUVWXY");
+  test_it("5a5A5b5B5d5D5e5E5f5F\n", 21, "ZZ[[]]^^__");
+  test_it("60616263646566676869\n", 21, "`abcdefghi");
+  test_it("6a6A6b6B6c6C6d6D6e6E6f6F\n", 25, "jjkkllmmnnoo");
+  test_it("70717273747576777879\n", 21, "pqrstuvwxy");
+  test_it("7a7A7b7B7c7C7d7D7e7E\n", 21, "zz{{||}}~~");
 
   return 0;
 }
